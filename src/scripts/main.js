65ced1491b892ecf99e1ce4aa3f2543e6fc700d5
Willian Briski <wyll_gustavo11@hotmail.com>
2024-03-19 09:58:37 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('sup');
+AOS.init();
+
+const dataDoEvento = new Date("Mar 26, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const HorasAteEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutosEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${HorasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado!'
+    }
+}, 1000);
